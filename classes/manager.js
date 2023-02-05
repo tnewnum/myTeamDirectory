9f785ddc052afeb79fdb19c3670c3242bd3e0a91
Thomas Newnum <thomas.newnum@gmail.com>
2023-02-05 17:18:59 -0500

    code works, gotta make it pretty and run some test

--- classes/manager.js
+++ classes/manager.js
@@ -2,13 +2,13 @@ const Employee = require('./employee.js')
 
 //manager - name, emoloyee id, email address, office number 
 class Manager extends Employee {
-    constructor(name, id, email, officeNum) {
+    constructor(name, id, email, officeNumber) {
        super(name, id, email)
-        this.officeNum = officeNum
+        this.officeNumber = officeNumber
     }
 
     getRole() {
-      return 'Manger'
+      return 'Manager'
     }
 }
 
--- html/htmlTemplate.js
+++ html/htmlTemplate.js
@@ -5,8 +5,8 @@
     <div class = "container">
         <div class = "manager">
         <h4>Manager</h4>
-        <h5 class="name>${name}</h5>
-        <h6 class="id>ID: ${id}</h6>
+        <h5 class="name">${name}</h5>
+        <h6 class="id">Employee ID: ${id}</h6>
         <h6 class="email">Email: <a href="mailto:${email}">${email}</a></h6>
         <h6 class="office">Office Number: ${officeNumber}</h6>        
         </div>
@@ -22,10 +22,11 @@ const engineerCard = (name, id, email, github) => {
     <div class = "container">
         <div class = "engineer">
         <h4>Engineer</h4>
-        <h5 class="name>${name}</h5>
-        <h6 class="id>ID: ${id}</h6>
+        <h5 class="name">${name}</h5>
+        <h6 class="id">Employee ID: ${id}</h6>
         <h6 class="email">Email: <a href="mailto:${email}">${email}</a></h6>
-        <h6 class="office">GitHub: @ ${github}</h6>        
+        <h6 class="handle">GitHub username @${github}</h6>
+        <h6 class="Github">GitHub Webpage: <a href="https://github.com/${github}">${github}</a></h6>        
         </div>
     </div>
     </section>
@@ -39,8 +40,8 @@ const internCard = (name, id, email, school) => {
     <div class = "container">
         <div class = "intern">
         <h4>Intern</h4>
-        <h5 class="name>${name}</h5>
-        <h6 class="id>ID: ${id}</h6>
+        <h5 class="name">${name}</h5>
+        <h6 class="id">Employee ID: ${id}</h6>
         <h6 class="email">Email: <a href="mailto:${email}">${email}</a></h6>
         <h6 class="office">I attended school at: ${school}</h6>        
         </div>
@@ -49,49 +50,68 @@ const internCard = (name, id, email, school) => {
     `
 }
 
-//if statement to determine what card 
+//if statement to determine what card to return 
 function generateCards (myTeam) {
+  let cards = [];
 
-  let allCards = ''
-  for (let index = 0; index < myTeam.length; index++) {
-    const element = array[index];
-
-   
-    
-  }
-    if (employee === 'Manager') {
-      return myTeam.officeNumber
-    } if (employee === 'Engineer') {
-     return engineerCard
-    } if (employee === 'Intern') {
-      return internCard
+  myTeam.forEach(member => {
+    if (member.getRole() === 'Manager') {
+      cards.push(managerCard(member.name, member.id, member.email, member.officeNumber))
+    } if (member.getRole() === 'Engineer') {
+      cards.push(engineerCard(member.name, member.id, member.email, member.github))
+    } if (member.getRole() === 'Intern') {
+      cards.push(internCard(member.name, member.id, member.email, member.school))
     }
-  }; 
+  });
+
+  return cards.join('');
+}
+
+
+
+
+
 
 
 //export funciton to generate HTML
 const generateHTML = (myTeam) =>
-  `<!DOCTYPE html>
+`<!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>myTeamDirectory</title>
-  
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<title>myTeamDirectory</title>
+
 </head>
 
 
 <body>
-    <header>
-    <h1>My Devloper Team</h1>
-    </header>
+<header class ="header">
+<h1>My Devloper Team</h1>
+</header>
 
-    <main>
-        <div>
-        ${managerCard(myTeam[0].name, myTeam[0].id, myTeam[0].email, myTeam[0].officeNum)}
-        </div>
-    </main>   
- </body>
+<main>
+<div>
+${generateCards(myTeam)}
+</div>
+</main>   
+</body>
 </html>`;
 
 module.exports = generateHTML;
+
+
+
+// ${managerCard(myTeam[0].name, myTeam[0].id, myTeam[0].email, myTeam[0].officeNumber)}
+// ${engineerCard(myTeam[1].name, myTeam[1].id, myTeam[1].email, myTeam[1].github)}
+// ${internCard(myTeam[2].name, myTeam[2].id, myTeam[2].email, myTeam[2].school)}
+
+
+// // let allCards = ''
+// // let employee = []
+// for (let i = 0; i < myTeam.length; i++) {
+  
+//    console.log(myTeam.element[i]);  
+  
+// }
